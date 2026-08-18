4985b011732f501f3de161339d8d9adee152654d
Rafael Panfil <rfpanfil@gmail.com>
2026-08-18 06:31:14 -0300

    fix: change style.css to index.css in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // File: App.tsx
 import React, { useEffect } from 'react';
-import './style.css';
+import './index.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { DataProvider } from './contexts/DataContext';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
